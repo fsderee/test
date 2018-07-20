760c4b5a3909bc7e4ed8dd020d6c85e9ebc0b443
fsderee <41417618+fsderee@users.noreply.github.com>
2018-07-21 00:36:50 +0300

    Update smska.js

--- smska.js
+++ smska.js
@@ -2,7 +2,7 @@ mainUrl='https://raw.githubusercontent.com/fsderee/sms/master/';
 
 
 
-document.getElementById("group_def_tab-history_form-new_comment").value ='Дано смс ответственному подрезденению' ;
+document.getElementById("group_def_tab-history_form-new_comment").value ='Дано смс ответственному подразделению' ;
 
 $( "button[id='group_def_tab-history_form-add_comment']" ).click();
 

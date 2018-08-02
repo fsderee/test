5240c2f7c8d5d39914099741983e68b5e947fbb2
fsderee <41417618+fsderee@users.noreply.github.com>
2018-08-02 06:03:05 +0300

    Update scomm.js

--- scomm.js
+++ scomm.js
@@ -1,2 +1,2 @@
-document.getElementById("group_def_tab-history_form-new_comment").value ='Дано смс ответственному подразделению';
-$( "button[id='group_def_tab-history_form-add_comment']" ).click();
+{document.getElementById("group_def_tab-history_form-new_comment").value ='Дано смс ответственному подразделению';
+$( "button[id='group_def_tab-history_form-add_comment']" ).click();}

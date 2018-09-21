5bd0f07edb8fcb73b785d591faf2a714d1b5a338
fsderee <41417618+fsderee@users.noreply.github.com>
2018-09-21 17:07:29 +0300

    Update gp2s.0.4.js

--- gp2s.0.4.js
+++ gp2s.0.4.js
@@ -20,19 +20,19 @@ if (!$( "div[class='ui-grid-col-9']" )[6]) {
         minutes = date.getMinutes();
     day < 10 && (day = "0" + day), month < 10 && (month = "0" + month), minutes < 10 && (minutes = "0" + minutes), hour < 10 && (hour = "0" + hour);
 
-  //  setTimeout(function(){
-      //  if (document.getElementById("group_interaction_info_form-tab_view-node_rule_building-address_ac").value.indexOf('Саратовская ОБЛ') > 1) {
-      //      reg = 'Саратовская ОБЛ';
-        //    setTimeout(function() {
-        //    $( "tr[data-label='ОМССиИС Саратов']" )[0].click();
-       //     }, 1000);
+    setTimeout(function(){
+        if (document.getElementById("group_interaction_info_form-tab_view-node_rule_building-address_ac").value.indexOf('Саратовская ОБЛ') > 1) {
+            reg = 'Саратовская ОБЛ';
+            setTimeout(function() {
+            $( "tr[data-label='ОМССиИС Саратов']" )[0].click();
+            }, 1000);
         }   
-       // if (document.getElementById("group_interaction_info_form-tab_view-node_rule_building_out").value.indexOf('Нижегородская ОБЛ') > 1) {
-        //    reg = 'Нижегородский филиал';
-	//		 if (document.getElementById("group_interaction_info_form-tab_view-node_rule_building-node_rule_building_input").value.indexOf('Нижний Новгород Г') > 1) {
-	//			$( "tr[data-label='ОМССиИС Нижний Новгород']" )[0].click();
-		//	 }
-      //  }
+        if (document.getElementById("group_interaction_info_form-tab_view-node_rule_building_out").value.indexOf('Нижегородская ОБЛ') > 1) {
+            reg = 'Нижегородский филиал';
+			 if (document.getElementById("group_interaction_info_form-tab_view-node_rule_building-node_rule_building_input").value.indexOf('Нижний Новгород Г') > 1) {
+				$( "tr[data-label='ОМССиИС Нижний Новгород']" )[0].click();
+			 }
+        }
     var text = "Аварийная ситуация на сети ПАО Ростелеком,  проводятся аварийно-восстановительные работы. Не доступны услуги (доступ в Интернет, Интерактивного ТВ, ) для  абонентов " + document.getElementById("group_interaction_info_form-tab_view-node_rule_building-node_rule_building_input").value;
         document.getElementById("group_interaction_info_form-tab_view-businessInteraction").value = text, document.getElementById("group_interaction_info_form-tab_view-commentary").value = text + " Предполагаемое время восстановления сервисов " + hour           +            ":" + minutes + " " + day + "." + month + "." + year    }, 1000);
    }

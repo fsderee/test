b214e829c2b42b634de02ffadc385a95e22e1014
fsderee <41417618+fsderee@users.noreply.github.com>
2019-01-17 00:57:42 +0300

    Update gpsvet.js

--- gpsvet.js
+++ gpsvet.js
@@ -2,7 +2,7 @@ document.getElementById("group_def_tab-history_form-new_comment").value ='Отк
 $( "button[id='group_def_tab-history_form-add_comment']" ).click();
 setTimeout(function() {
   $( "button[id='gi_header-assignTask']" ).click();
-  }, 1000);
+  }, 10000);
 
 setTimeout(function() {
   document.getElementById('reassign_dialog_form-j_idt1943_6').click();

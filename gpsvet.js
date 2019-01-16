5e554895bccb24afcfaa5c8c3022fa2f5bfe48ec
fsderee <41417618+fsderee@users.noreply.github.com>
2019-01-16 23:46:59 +0300

    Update gpsvet.js

--- gpsvet.js
+++ gpsvet.js
@@ -1,5 +1,5 @@
 $( "button[id='gi_header-assignTask']" ).click();
 document.getElementById("group_def_tab-history_form-new_comment").value ='Отключение эл. энергии' ;
 $( "button[id='group_def_tab-history_form-add_comment']" ).click();
-document.getElementById('reassign_dialog_form-j_idt1943').click();
+document.getElementById('ui-helper-hidden-accessible').click();
 document.getElementById('reassign_dialog_form-j_idt1943_6').click();

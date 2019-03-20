44539386a8ec7f6e0e2a6ba44b31ec87ba36f365
fsderee <41417618+fsderee@users.noreply.github.com>
2019-03-20 10:01:57 +0300

    Update gpsvet.js

--- gpsvet.js
+++ gpsvet.js
@@ -14,6 +14,10 @@ setTimeout(function() {
 setTimeout(function() {
   document.getElementById('reassign_dialog_form-j_idt1969_6').click();
   }, 2000);
+
+setTimeout(function() {
+  document.getElementById('reassign_dialog_form-j_idt1970_6').click();
+  }, 2000);
 setTimeout(function() {
   $( "button[id='reassign_dialog_form-reassign']" ).click();
    }, 2000);

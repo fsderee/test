8eeed536002787beac464068652dc11a2d16d677
fsderee <41417618+fsderee@users.noreply.github.com>
2019-06-18 02:19:42 +0300

    Update smska2.js

--- smska2.js
+++ smska2.js
@@ -17,7 +17,7 @@ arr.forEach(function(element) {
 var adres = $('#group_interaction_info_form-tab_view-node_rule_building_out-address_ac_out').text();
 var timedown = $('#group_interaction_info_form-tab_view-calculate_due_date_form_out').text();
 var namberGP = $( "span[class='heading-accent']" ).text();
-var addre2 = $( 'xpath["//*[@id="group_interaction_info_form-tab_view-group_interaction_rule_table_data"]/tr/td[1]/text()"]' ).text();
+var addre2 = $('//*[@id="group_interaction_info_form-tab_view-group_interaction_rule_table_data"]/tr/td[1]/text()').text();
 
 var vse = new Array();
 var len = $(".ui-datatable-selectable").length;
